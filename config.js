e3fde5ddcac4eeb21a94d08e8ec62b3d9d9e20e6
naveen-ku <naveen2951996@gmail.com>
2019-12-30 02:24:14 +0530

    Uploading cryptocurrency project

---
+++ config.js
@@ -0,0 +1,11 @@
+// making genesis block 
+//to achieve this we are creating the genesis data (dummy data)
+
+const GENESIS_DATA = {
+    timestamp: 1,
+    lastHash: '-----',
+    hash: 'hash-one',
+    data: []
+};
+
+module.exports = { GENESIS_DATA };
