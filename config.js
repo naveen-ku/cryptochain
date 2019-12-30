61812eada9c5daed7a3b37e320fcc8137a5d644b
naveen-ku <naveen2951996@gmail.com>
2019-12-30 15:52:25 +0530

    adding code for implementing nonce and difficulty level(static) for blocks mininng

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 // making genesis block 
 //to achieve this we are creating the genesis data (dummy data)
 
+const INITIAL_DIFFICULTY = 3;
 const GENESIS_DATA = {
     timestamp: 1,
     lastHash: '-----',
     hash: 'hash-one',
+    difficulty : INITIAL_DIFFICULTY,
+    nonce : 0,
     data: []
 };
 
