8ca9f8c894aab111776d9e40fdc39422c3d57ae4
naveen-ku <naveen2951996@gmail.com>
2020-01-01 16:50:33 +0530

    Fixed bug(Adding the feature of synchronization): If we start a new peer it does not catch the current blockchain i.e. is not in sync with the main blockchain.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
 
@@ -7,7 +8,9 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
 
 app.use(bodyParser.json());
 
@@ -26,7 +29,17 @@ app.post('/api/mine', (req, res) =>{
 
 });
 
-const DEFAULT_PORT = 3000;
+const syncChain = () =>{
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body ) => {
+        if(!error && response.statusCode === 200){
+            const rootChain = JSON.parse(body);
+
+            console.log('replace chain on a sync with', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    })
+};
+
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT === 'true'){
@@ -36,4 +49,9 @@ if(process.env.GENERATE_PEER_PORT === 'true'){
 
 
 const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => console.log(`listening at localhost: ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`listening at localhost: ${PORT}`);
+    if(PORT !== DEFAULT_PORT){
+        syncChain();
+    }
+});
