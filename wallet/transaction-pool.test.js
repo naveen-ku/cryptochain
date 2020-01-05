c23889cdb10103f2513c8c0ddd1a4d76c696fcb7
naveen-ku <naveen2951996@gmail.com>
2020-01-05 10:49:30 +0530

    Added the following features:- Created the core transaction miner class to capture how miners should add transactions to the blockchain.  Added the ability to grab valid transactions from the transaction pool.Added a way to clear blockchain transactions to ensure that only unique transaction objects could be recorded.
    Added a mining transactions endpoint to enable transaction mining through the API.

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,6 +1,7 @@
 const TransactionPool = require('./transaction-pool');
 const Transaction = require('./transaction');
 const Wallet = require('./index');
+const  Blockchain = require('../blockchain');
 
 describe('TransactionPool', () =>{
     let transactionPool, transaction, senderWallet;
@@ -33,4 +34,73 @@ describe('TransactionPool', () =>{
         });
     });
 
+    describe('validTransacation()', () => {
+        let validTransactions , errorMock;
+
+        beforeEach(() =>{
+            validTransactions = [];
+            errorMock = jest.fn();
+            global.console.error = errorMock;
+
+            for(let i = 0; i <10; i++){
+                transaction = new Transaction({
+                    senderWallet,
+                    recipient: 'any-recipient',
+                    amount: 30
+                });
+    
+                if(i%3===0){
+                    transaction.input.amount = 99999;
+                }else if(i%3===1){
+                    transaction.input.signature = new Wallet().sign('foo');
+                }else{
+                    validTransactions.push(transaction)
+                }
+    
+                transactionPool.setTransaction(transaction);
+            }
+        });
+
+        it('returns valid transaction', () => {
+            expect(transactionPool.validTransactions()).toEqual(validTransactions);
+        });
+
+        it('logs error for the transactions', () => {
+            transactionPool.validTransactions();
+            expect(errorMock).toHaveBeenCalled();
+        });
+    });
+
+    describe('clear()', () => {
+        it('clears the transaction', () => {
+            transactionPool.clear();
+
+            expect(transactionPool.transactionMap).toEqual({});
+        });
+    });
+
+    describe('clearBlockchainTransactions()', () =>{
+        it('clears the pool of any existing blockchain transactions', () =>{
+            const blockchain = new Blockchain();
+            const expectedTransactionMap = {};
+
+            for(let i=0; i<6; i++){
+                const transaction = new Wallet().createTransaction({
+                    recipient: 'foo', amount: 20
+                });
+                
+                transactionPool.setTransaction(transaction);
+
+                if(i%2===0){
+                    blockchain.addBlock({ data: [transaction] })
+                }else{
+                    expectedTransactionMap[transaction.id] = transaction;
+                }
+            }
+
+            transactionPool.clearBlockchainTransaction({ chain: blockchain.chain });
+
+            expect(transactionPool.transactionMap).toEqual(expectedTransactionMap);
+        });
+    })
 });
