1740e2b522c53f1f10e90d7bc329bfd62a80374a
naveen-ku <naveen2951996@gmail.com>
2020-01-07 09:53:12 +0530

    Adding more frontend features

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Blocks from './Blocks';
+import logo from '../assets/logo.png';
+
 
 class App extends Component{ 
     state ={ walletInfo: {} };
@@ -12,12 +15,19 @@ class App extends Component{
     render(){
         const { address, balance } = this.state.walletInfo;
         return(
-            <div>
+            <div className='App'>
+            <img className = 'logo' src ={logo}></img>
+            <br />
                 <div>
                 Welcome to the blockchain...
                 </div>
-                <div>Address: {address}</div>
-                <div>Balance: {balance}</div>
+                <br />
+                <div className = 'WalletInfo'>
+                    <div>Address: {address}</div>
+                    <div>Balance: {balance}</div>
+                </div>
+                <br />
+                <Blocks />
             </div>
 
         );
