dbbd7047b9d6d1595a2d70a0f559389250d444b4
naveen-ku <naveen2951996@gmail.com>
2020-01-07 14:07:11 +0530

    Adding the feature of mining a block

--- client/src/components/TransactionPool.js
+++ client/src/components/TransactionPool.js
@@ -1,6 +1,8 @@
 import React , { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import Transaction from './Transaction';
 import { Link } from 'react-router-dom';
+import history from '../history';
 import { transform } from '@babel/core';
 
 //Time is milliseconds
@@ -13,36 +15,50 @@ class TransactionPool extends Component {
         fetch(`${document.location.origin}/api/transaction-pool-map`)
             .then(response => response.json())
             .then(json => this.setState({ transactionPoolMap: json }));
-        }
-
-        componentDidMount() {
+    }
+    
+    fetchMineTransactions = () => {
+        fetch(`${document.location.origin}/api/mine-transactions`)
+            .then(response => {
+                if(response.status === 200) {
+                    alert('success');
+                    history.push('/blocks');
+                }else{
+                    alert('The mine-transactions block request did not complete.');
+                }
+            });
+    } 
+    
+    componentDidMount() {
             this.fetchTransactionPoolMap();
 
              this.fetchPoolMapInterval = setInterval(() => this.fetchTransactionPoolMap(), POLL_INTERVAL_MS);
-        }
+    }
 
-        componentWillUnmount(){
+    componentWillUnmount(){
             clearInterval(this.fetchPoolMapInterval);
-        }
+    }
 
-        render() {
-            return(
-                <div className='TransactionPool'>
-                    <div><Link to='/'>Home</Link></div>
-                    <h3>Transaction Pool</h3>
-                    {
-                        Object.values(this.state.transactionPoolMap).map(transaction => {
-                            return(
-                                <div key={transaction.id}>
-                                    <hr />
-                                    <Transaction transaction={transaction } />
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            )
-        }
+    render() {
+        return(
+            <div className='TransactionPool'>
+                <div><Link to='/'>Home</Link></div>
+                <h3>Transaction Pool</h3>
+                {
+                    Object.values(this.state.transactionPoolMap).map(transaction => {
+                        return(
+                            <div key={transaction.id}>
+                                <hr />
+                                <Transaction transaction={transaction } />
+                            </div>
+                        )
+                    })
+                }
+                <hr />
+                <Button bsStyle="danger" onClick={this.fetchMineTransactions}>Mine the Transactions</Button>
+            </div>
+        )
+    }
 }
 
 export default TransactionPool;
